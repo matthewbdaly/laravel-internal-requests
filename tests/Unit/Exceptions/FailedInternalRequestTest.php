f6d0d5bbac27077f7493bd7c0913f59000399be7
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-09-20 21:10:26 +0100

    Added failing test for JSON responses

--- tests/Unit/Exceptions/FailedInternalRequestTest.php
+++ tests/Unit/Exceptions/FailedInternalRequestTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Mockery as m;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Matthewbdaly\LaravelInternalRequests\Exceptions\FailedInternalRequestException;
 
 class FailedInternalRequestTest extends TestCase
@@ -28,6 +29,17 @@ class FailedInternalRequestTest extends TestCase
             'HTTP_Accept'             => 'application/json',
         ]);
         $response = Response::create();
+        $exception = new FailedInternalRequestException($request, $response);
+        $this->assertEquals($response, $exception->getResponse());
+    }
+
+    /** @test */
+    public function it_handles_a_json_response()
+    {
+        $request = Request::create('/api/endpoint', 'POST', [], [], [], [
+            'HTTP_Accept'             => 'application/json',
+        ]);
+        $response = JsonResponse::create();
         $exception = new FailedInternalRequestException($request, $response);
         $this->assertEquals($response, $exception->getResponse());
     }
